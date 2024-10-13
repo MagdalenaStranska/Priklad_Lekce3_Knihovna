741bf516c0ed25fca1a4c11e44e0a3d4e1a8821e
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-10-13 19:17:36 +0200

    Pridavam reseni

---
+++ script.js
@@ -0,0 +1,26 @@
+// 1. V JavaScriptovém programu vytvořte objekt představující jednu knihu v knihovně. Uvažte, jaké vlastnosti může taková kniha mít. Rozhodně budeme chtít název, autora a počet stran. Přidejte do objektu alespoň tři další vlastnosti tak, aby obsahovaly čísla, řetězce i vnořený objekt.
+// 2. Vytvořte alespoň jednu další knihu se stejnými vlastnostmi ale jinými hodnotami.
+
+const book1 = {
+    nazev: 'Pilíře země',
+    nazevOriginalu: 'The Pillars of the Earth',
+    autor: 'Ken Follett',
+    numberOfPages: 926,
+    datumVydani: {
+        original: 1989,
+        ceskyPreklad: 2009,
+    }
+    ISBN: '978-80-242-2410-7',
+}
+
+const book1 = {
+    nazev: 'Na věky věků',
+    nazevOriginalu: 'World Without End',
+    autor: 'Ken Follett',
+    numberOfPages: 928,
+    datumVydani: {
+        original: 2007,
+        ceskyPreklad: 2009,
+    }
+    ISBN: '978-80-242-2382-7',
+}
